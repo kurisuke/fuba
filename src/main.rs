756e07e8a9621318202c79ae6c1bced70633037f
Peter Helbing <peter@abulafia.org>
2018-06-02 23:39:49 +0900

    multirun: make multithreaded

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate clap;
+extern crate num_cpus;
 extern crate petgraph;
 extern crate rand;
 #[macro_use]
@@ -18,25 +19,30 @@ fn main() {
         .author(crate_authors!())
         .about("Simulate football (soccer) matches and tournaments")
         .args_from_usage(
-            "-s, --simulate=[N] 'Simulate N runs and print statistics'
+            "-s, --simulate=[N]    'Simulate N runs and print statistics'
+             -n, --num-threads=[N] 'Number of Threads (for simulation mode)'
              <CONFIG>           'Configuration file (toml)'",
         )
         .get_matches();
 
     let config_file = a.value_of("CONFIG").unwrap();
 
-    let mut rng = rand::thread_rng();
-    let mut sim = sim::Sim::new(&mut rng);
-
-    let config = config::read_config(&config_file).unwrap();
-
     match a.value_of("simulate") {
         Some(n_str) => {
             let n: u32 = n_str.parse().unwrap();
-            println!("Launch multirun mode, {} runs", n);
-            ::multirun::multirun(config, &mut sim, n);
+            let num_threads: u32 = match a.value_of("num-threads") {
+                Some(x) => x.parse().unwrap_or(1),
+                None => num_cpus::get() as u32,
+            };
+            println!("Launch multirun mode, {} runs, {} threads", n, num_threads);
+            ::multirun::multirun(String::from(config_file), n, num_threads);
         }
         None => {
+            let mut rng = rand::thread_rng();
+            let mut sim = sim::Sim::new(&mut rng);
+
+            let config = config::read_config(&config_file).unwrap();
+
             let round_results = result::calc(config, &mut sim);
             for r in round_results {
                 r.print();
--- src/multirun.rs
+++ src/multirun.rs
@@ -1,17 +1,43 @@
+use rand;
 use std::collections::HashMap;
+use std::sync::mpsc;
+use std::thread;
 
-pub fn multirun(config: ::config::Config, sim: &mut ::sim::Sim, n: u32) {
+pub fn multirun(config_file: String, n: u32, num_threads: u32) {
     let mut winners = HashMap::<String, u32>::new();
 
-    for _i in 0..n {
-        let c = config.clone();
-        let round_results = ::result::calc(c, sim);
+    let (tx, rx) = mpsc::channel();
 
-        let final_round = round_results.last().unwrap();
-        let w = winners
-            .entry(final_round.stats[0].team.borrow().name.clone())
-            .or_insert(0);
+    let mut thread_handles = vec![];
+
+    for _i in 0..num_threads {
+        let local_tx = mpsc::Sender::clone(&tx);
+        let local_config_file = config_file.clone();
+        thread_handles.push(thread::spawn(move || {
+            let mut rng = rand::thread_rng();
+            let mut sim = ::sim::Sim::new(&mut rng);
+
+            let config = ::config::read_config(&local_config_file).unwrap();
+
+            for _i in 0..(n / num_threads) {
+                let c = config.clone();
+                let round_results = ::result::calc(c, &mut sim);
+
+                let final_round = round_results.last().unwrap();
+                let winner = final_round.stats[0].team.borrow().name.clone();
+                local_tx.send(winner).unwrap();
+            }
+        }));
+    }
+    drop(tx);
+
+    for winner in rx {
+        let w = winners.entry(winner.clone()).or_insert(0);
         *w += 1;
+    }
+
+    for h in thread_handles.into_iter() {
+        h.join().unwrap();
     }
 
     let mut winners_v: Vec<_> = winners.into_iter().collect();
